ad175fb38b241829c37b92cd3e2bca2bca447854
Jake Jackson <jake.jackson@york.ac.uk>
2022-08-05 17:54:02 +0100

    wippinm

--- index.ts
+++ index.ts
@@ -1,11 +1,26 @@
-//const world = 'world';
-import core from '@actions/core';
-import github from '@actions/github'
-import {execa} from 'execa';
+import * as core from '@actions/core';
+import {exec } from '@actions/exec'
+
+const stages : string[] = ['npm install','npm run test','npm run lint','npm run build']
 
 async function run() {
+    stages.forEach(stage => {
+        runTask(stage)
+    });    
+}
 
-
+function runTask(operation: string) {
+    core.info(`executing: ${ operation }`)
+    try { 
+        exec(operation)
+    } 
+    catch (error) {
+        if (error instanceof(Error)) {
+            core.setFailed(error.message)
+        } else {
+            throw(error)
+        }
+    }
 }
 
 run()
